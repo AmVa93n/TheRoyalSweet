19635e69051036697964486329bd9707f99ca8bb
amirvaknin <amir6661@gmail.com>
2025-05-06 19:22:17 +0200

    removed authentication related components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,7 @@ import ShopPage from "./pages/ShopPage";
 import ProductPage from "./pages/ProductPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import AdminPage from "./pages/AdminPage";
-import SignupPage from "./pages/SignupPage";
-import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
-//import IsPrivate from "./components/auth/IsPrivate";
-import IsAnon from "./components/auth/IsAnon";
 
 function App() {
   return (
@@ -21,23 +17,6 @@ function App() {
         <Route path="/product/:productId" element={<ProductPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/admin" element={<AdminPage />} />
-
-        <Route
-          path="/signup"
-          element={
-            <IsAnon>
-              <SignupPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <IsAnon>
-              <LoginPage />
-            </IsAnon>
-          }
-        />
       </Routes>
     </div>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,24 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
-import { AuthProviderWrapper } from "../../client/src/context/auth.context";
 import { LanguageProvider } from "../../client/src/context/language.context";
 import { CartProvider } from "../../client/src/context/cart.context";
-import '@fontsource/montserrat';
+import '@fontsource/montserrat.css';
 import { ThemeProvider } from "../../client/src/context/theme.context";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProviderWrapper>
-        <ThemeProvider>
-          <LanguageProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </LanguageProvider>
-        </ThemeProvider>
-      </AuthProviderWrapper>
+      <ThemeProvider>
+        <LanguageProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </LanguageProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
