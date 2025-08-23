b8ff6d66634cc52d3a8b1f0eb4f2b2a795a05a2e
AmVa93 <amir6661@gmail.com>
2025-08-23 21:15:22 +0100

    moved order summary to separate component

--- client/src/pages/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage.tsx
@@ -9,11 +9,12 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs from 'dayjs';
 import type { Order } from '../types';
+import OrderSummary from '../components/OrderSummery';
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 function CheckoutPage() {
-    const { language, cart, setCart, setIsCartOpen } = useStore()
+    const { language, cart, setCart } = useStore()
     const [orderData, setOrderData] = useState<Omit<Order, '_id' | 'createdAt' | 'additionalIngredients'>>({
         name: '',
         email: '',
@@ -27,8 +28,6 @@ function CheckoutPage() {
         },
         items: cart
     });
-    const deliveryFee = orderData.pickup ? 0 : 5
-    const totalAmount = cart.reduce((sum, item) => sum + item.price * item.quantity, 0)
     const [clientSecret, setClientSecret] = useState('');
     const isFormValid = orderData.name && orderData.email && orderData.phone && (orderData.pickup || (orderData.shipping.address && orderData.shipping.city && orderData.shipping.zip));
     
@@ -179,50 +178,7 @@ function CheckoutPage() {
                 </div>
 
                 {/* Order Summary */}
-                <div className="bg-white shadow p-4 rounded h-fit">
-                    <div className='flex items-center justify-between'>
-                        <h2 className="text-lg font-semibold">
-                            {language === 'en' ? 'Order Summary' : 'Resumo do Pedido'} ({cart.reduce((sum, item) => sum + item.quantity, 0)})
-                        </h2>
-                        <button 
-                            className='py-1 px-2 rounded-sm bg-gray-200 hover:bg-gray-300 transition cursor-pointer text-sm'
-                            onClick={() => setIsCartOpen(true)}
-                        >
-                            {language === 'en' ? 'Edit cart' : 'Editar carrinho'}
-                        </button>
-
-                    </div>
-                    <hr className="my-4 border-gray-300" />
-                    {cart.map((item) => (
-                        <div key={item.product._id} className="flex items-center mb-3">
-                            <img
-                                src={item.product.images[0]}
-                                alt={item.product._id}
-                                className="w-10 h-10 object-cover mr-3"
-                            />
-                            <span className="flex-grow">{item.product.name[language]} x {item.quantity}</span>
-                            <span>{(item.price * item.quantity).toFixed(2).replace('.', ',')} €</span>
-                        </div>
-                    ))}
-                    <hr className="my-4 border-gray-300" />
-                    <div className="flex items-center mb-2">
-                        <span className="flex-grow">{language === 'en' ? 'Items' : 'Produtos'}</span>
-                        <span>{totalAmount.toFixed(2).replace('.', ',')} €</span>
-                    </div>
-                    <div className="flex items-center mb-2">
-                        <span className="flex-grow">{language === 'en' ? 'Delivery' : 'Entrega'}</span>
-                        <span>{deliveryFee.toFixed(2).replace('.', ',')} €</span>
-                    </div>
-                    <div className="flex items-center mb-2">
-                        <span className="flex-grow">{language === 'en' ? 'VAT' : 'IVA'}</span>
-                        <span>0,00 €</span>
-                    </div>
-                    <hr className="my-4 border-gray-300" />
-                    <div className="flex items-center">
-                        <h3 className="text-lg font-bold flex-grow">Total</h3>
-                        <span className="text-lg font-bold">{(totalAmount + deliveryFee).toFixed(2).replace('.', ',')} €</span>
-                    </div>
-                </div>
+                <OrderSummary orderData={orderData} />
             </div>
         </div>
     );
