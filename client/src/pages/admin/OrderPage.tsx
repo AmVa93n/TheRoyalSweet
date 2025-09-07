f08b588b76ca9ce55e8c1be07c3d79f9e998e2ac
amirvaknin <amir6661@gmail.com>
2025-09-07 22:07:50 +0200

    added additional ingredients section to order details

--- client/src/pages/admin/OrderPage.tsx
+++ client/src/pages/admin/OrderPage.tsx
@@ -147,10 +147,39 @@ export default function OrderPage() {
           </div>
         </div>
 
-        {/* Ingredients */}
+        {/* Additional Ingredients */}
         <div className="max-w-5xl mx-auto mt-10 bg-white rounded-2xl shadow-md p-8">
           <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            Total Ingredients
+            Additional Ingredients
+          </h2>
+          <div className="overflow-x-auto">
+            <table className="w-full border-collapse">
+              <thead className="bg-gray-100 text-gray-700">
+                <tr>
+                  <th className="px-4 py-2 text-left">Ingredient</th>
+                  <th className="px-4 py-2 text-center">Amount</th>
+                  <th className="px-4 py-2 text-center">Price / Unit</th>
+                  <th className="px-4 py-2 text-center">Total Price</th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {order.additionalIngredients.map((item) => (
+                  <tr key={item.ingredient._id} className="hover:bg-gray-50">
+                    <td className="px-4 py-2 text-gray-800">{item.ingredient.name}</td>
+                    <td className="px-4 py-2 text-center">{item.amount} {item.ingredient.recipeUnits}</td>
+                    <td className="px-4 py-2 text-center">{item.ingredient.pricePerUnit.toFixed(3)} €</td>
+                    <td className="px-4 py-2 text-center font-medium text-gray-800">{(item.ingredient.pricePerUnit * item.amount).toFixed(3)} €</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        {/* Ingredients Breakdown */}
+        <div className="max-w-5xl mx-auto mt-10 bg-white rounded-2xl shadow-md p-8">
+          <h2 className="text-xl font-semibold text-gray-800 mb-4">
+            Ingredients Breakdown
           </h2>
           <div className="overflow-x-auto">
             <table className="w-full border-collapse">
