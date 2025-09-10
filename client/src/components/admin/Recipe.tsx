8b59e3c87a38a64e1efae13b853a9193912279f4
amirvaknin <amir6661@gmail.com>
2025-09-11 00:47:58 +0200

    added supermarket share display for recipe

--- client/src/components/admin/Recipe.tsx
+++ client/src/components/admin/Recipe.tsx
@@ -1,10 +1,19 @@
 import type { Ingredient } from "../../types";
+import { supermarkets } from "../../utils";
 
 type Props = {
     recipe: { ingredient: Ingredient; amount: number }[];
 }
 
 export default function Recipe({ recipe }: Props) {
+    const supermarketList = Object.keys(supermarkets).sort((a, b) => getSupermarketShare(b) - getSupermarketShare(a)).filter(sm => getSupermarketShare(sm) > 0);
+  
+    function getSupermarketShare(supermarket: string) {
+      const totalIngredients = recipe.length;
+      const ingredientsInSupermarket = recipe.filter(item => item.ingredient.supermarkets.includes(supermarket)).length;
+      return (ingredientsInSupermarket / totalIngredients) * 100;
+    }
+
     return (
         <div className="max-w-5xl mx-auto mt-10 bg-white rounded-2xl shadow-md p-8">
           <h2 className="text-xl font-semibold text-gray-800 mb-4">Recipe</h2>
@@ -29,6 +38,18 @@ export default function Recipe({ recipe }: Props) {
                 ))}
               </tbody>
             </table>
+          </div>
+
+          <div className="flex flex-col gap-2 mt-6">
+            {supermarketList.map(sm => (
+              <div key={sm} className="flex items-center gap-4">
+                {supermarkets[sm] ? <img src={supermarkets[sm]} alt={sm} className="w-12 object-fit" /> : sm}
+                <div className="flex items-center gap-2 w-full">
+                  <div className={`h-2 bg-indigo-500 rounded`} style={{ width: `${getSupermarketShare(sm)}%` }}></div>
+                  <span className="text-sm text-gray-600">{Math.round(getSupermarketShare(sm))}%</span>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
     )
