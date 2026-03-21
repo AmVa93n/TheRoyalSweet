2d7dbc5a31fc7e4c1e2466171521dc98593c35c5
amirvaknin <amir6661@gmail.com>
2026-03-22 00:01:16 +0100

    added conveyer belt animation

--- app/components/ShopPreview.tsx
+++ app/components/ShopPreview.tsx
@@ -5,17 +5,23 @@ import Link from "next/link"
 
 function ShopPreview({ products }: { products: Product[] }) {
   const { language } = useStore();
+  const extendedProducts = [...products, ...products]; // loop trick
 
   return (
-    <section className="mx-auto py-10 scroll-mt-16" id='products'>
-      <h2 className="text-3xl text-center italic font-montserrat mb-8 text-[#593b3e]">
+    <section className="mx-auto py-10 scroll-mt-16 w-full" id='products'>
+      <h2 className="text-3xl text-center italic font-montserrat text-[#593b3e]">
         {language === 'en' ? 'Our Desserts' : 'Os Nossos Doces'}
       </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center mb-10 flex-wrap">
-        {products.map((product) => (
-            <ProductCard product={product} key={product._id} />
-        ))}
+      {/* Conveyor belt */}
+      <div className="overflow-hidden py-8 relative">
+        <div className="flex gap-8 w-max animate-scroll">
+          {extendedProducts.map((product, index) => (
+            <ProductCard product={product} width={320} key={`${product._id}-${index}`} />
+          ))}
+        </div>
+        <div className="pointer-events-none absolute top-0 left-0 h-full w-24 bg-gradient-to-r from-[#e6dcd5] to-transparent z-10" />
+        <div className="pointer-events-none absolute top-0 right-0 h-full w-24 bg-gradient-to-l from-[#e6dcd5] to-transparent z-10" />
       </div>
 
       <Link
