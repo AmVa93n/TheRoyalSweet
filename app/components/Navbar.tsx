523fe6ba2f2f78fec066b0658937e518e82bda9d
amirvaknin <amir6661@gmail.com>
2026-03-22 18:14:08 +0100

    moved shop link to front

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,10 +18,10 @@ function Navbar() {
   const pathname = usePathname();
 
   const navLinks = [
+    { text: language === 'en' ? 'Shop' : 'Loja', route: '/shop', icon: <StorefrontIcon size={24} /> },
     { text: language === 'en' ? 'About me' : 'Sobre mim', route: '/', sectionId: 'aboutme', icon: <ChefHatIcon size={24} /> },
     { text: language === 'en' ? 'Orders' : 'Encomendas', route: '/', sectionId: 'how-to-order', icon: <ShoppingBagIcon size={24} /> },
     { text: language === 'en' ? 'Contacts' : 'Contactos', route: '/', sectionId: 'contacts', icon: <PhoneIcon size={24} /> },
-    { text: language === 'en' ? 'Shop' : 'Loja', route: '/shop', icon: <StorefrontIcon size={24} /> },
     { text: language === 'en' ? 'Menu' : 'Preçário', route: '/menu.pdf', external: true, icon: <ForkKnifeIcon size={24} /> },
     { text: language === 'en' ? 'Blog' : 'Blogue', route: `https://theroyalsweet.com/${language === 'en' ? 'en/' : ''}`, external: true, icon: <PencilCircleIcon size={24} /> },
   ];
